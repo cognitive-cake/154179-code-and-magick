fc468a94305395d4111c84c2a0c37b783aae909b
cognitive-cake <nd.chernikov@gmail.com>
2017-08-14 21:41:12 +0300

    Переформатировал stat.js

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,18 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  var histoParameters = {
+    histoStartX: 120,
+    histoStartY: 200,
+    histoHeight: 150,
+    columnWidth: 40,
+    spaceBetweenColumns: 50,
+    userColumnColor: 'rgba(255, 0, 0, 1)',
+    otherPlayersColor: 'blue'
+  };
+
   // Геометрия окна статистики
-  var drawStatisticsRectangle = function () {
+  function drawStatisticsRectangle() {
     ctx.beginPath();
     ctx.moveTo(100, 30);
     ctx.lineTo(100, 260);
@@ -13,46 +23,41 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.lineTo(500, 10);
     ctx.lineTo(120, 10);
     ctx.closePath();
-  };
+  }
 
-  // Тень окна статистики
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowOffsetY = 10;
-  ctx.shadowOffsetX = 10;
+  function drawStatisticsWindow() {
+    // Тень окна статистики
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+    ctx.shadowOffsetY = 10;
+    ctx.shadowOffsetX = 10;
 
-  // Фон окна статистики
-  ctx.fillStyle = '#6d86af';
-  drawStatisticsRectangle();
-  ctx.fill();
+    // Фон окна статистики
+    ctx.fillStyle = '#6d86af';
+    drawStatisticsRectangle();
+    ctx.fill();
 
-  // Возвращение значений по-умолчанию для тени
-  ctx.shadowOffsetY = 0;
-  ctx.shadowOffsetX = 0;
+    // Возвращение значений по-умолчанию для тени
+    ctx.shadowOffsetY = 0;
+    ctx.shadowOffsetX = 0;
+  }
 
-  // Рамка окна статистики
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = '#3d444f';
-  drawStatisticsRectangle();
-  ctx.stroke();
+  function drawStatisticsBorder() {
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#3d444f';
+    drawStatisticsRectangle();
+    ctx.stroke();
+  }
 
-  // Заголовок
-  ctx.font = '16px "PT Mono"';
-  ctx.textAlign = 'center';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 310, 40);
-  ctx.fillText('Список результатов:', 310, 60);
+  function drawStatisticsTitle() {
+    ctx.font = '16px "PT Mono"';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = '#000';
+    ctx.fillText('Ура вы победили!', 310, 40);
+    ctx.fillText('Список результатов:', 310, 60);
+  }
 
   // Гистограмма
-  var histoParameters = {
-    histoStartX: 120,
-    histoStartY: 200,
-    histoHeight: 150,
-    columnWidth: 40,
-    spaceBetweenColumns: 50,
-    userColumnColor: 'rgba(255, 0, 0, 1)',
-    otherPlayersColor: 'blue'
-  };
-  var drawHistogram = function () {
+  function drawHistogram() {
     var maxTime = -1;
 
     for (var i = 0; i < times.length; i++) {
@@ -64,7 +69,7 @@ window.renderStatistics = function (ctx, names, times) {
 
     var columnStep = histoParameters.histoHeight / Math.round(maxTime);
 
-    var drawHistoColumns = function () {
+    function drawHistoColumns() {
       var h = histoParameters;
 
       for (i = 0; i < times.length; i++) {
@@ -74,9 +79,12 @@ window.renderStatistics = function (ctx, names, times) {
         ctx.fillRect(h.histoStartX + i * (h.columnWidth + h.spaceBetweenColumns), h.histoStartY, h.columnWidth, -(currTime * columnStep.toFixed(3)));
         ctx.globalAlpha = 1;
       }
-
-    };
+    }
     drawHistoColumns();
-  };
+  }
+
+  drawStatisticsWindow();
+  drawStatisticsBorder();
+  drawStatisticsTitle();
   drawHistogram();
 };
