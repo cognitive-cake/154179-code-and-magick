c5c6aad12e9a21269df5b8200fa4052ccb286806
Nick-ch <nd.chernikov@gmail.com>
2017-08-15 01:30:20 +0300

    Закончил выводить значения в объекты

--- js/stat.js
+++ js/stat.js
@@ -8,13 +8,21 @@ window.renderStatistics = function (ctx, names, times) {
     columnWidth: 40,
     spaceBetweenColumns: 50,
     userColumnColor: 'rgba(255, 0, 0, 1)',
-    otherPlayersColor: 'blue'
+    otherPlayersColor: 'blue',
+    playerName: 'Вы',
+    histoPrecision: 2,
+    opacityMin: 0.2,
+    opacityMax: 1,
+    opacityDefault: 1
   };
   var windowGeometry = {
     startX: 100,
     startY: 10,
     width: 420,
     height: 270,
+    get centerX() { // Вот про это я ничего не знаю =) Просто нагуглил ))
+      return this.width / 2 + this.startX;
+    },
     chamfer: 20,
     borderWidth: 3,
     backgroundColor: '#e2e191',
@@ -22,9 +30,12 @@ window.renderStatistics = function (ctx, names, times) {
     shadowColor: 'rgba(0, 0, 0, 0.7)',
     shadowOffsetX: 10,
     shadowOffsetY: 10,
+    shadowOffsetDefault: 0,
     titleFont: '16px "PT Mono"',
     titleFontAlign: 'center',
-    titleFontColor: '#000'
+    titleFontColor: '#000',
+    titleFirstLineY: 40,
+    titleSecondLineY: 60
   };
 
   // Геометрия окна статистики
@@ -42,19 +53,16 @@ window.renderStatistics = function (ctx, names, times) {
   }
 
   function drawStatisticsWindow() {
-    // Тень окна статистики
     ctx.shadowColor = windowGeometry.shadowColor;
     ctx.shadowOffsetY = windowGeometry.shadowOffsetY;
     ctx.shadowOffsetX = windowGeometry.shadowOffsetX;
 
-    // Фон окна статистики
     ctx.fillStyle = windowGeometry.backgroundColor;
     drawStatisticsRectangle(windowGeometry);
     ctx.fill();
 
-    // Возвращение значений по-умолчанию для тени
-    ctx.shadowOffsetY = 0;
-    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = windowGeometry.shadowOffsetDefault;
+    ctx.shadowOffsetX = windowGeometry.shadowOffsetDefault;
   }
 
   function drawStatisticsBorder() {
@@ -68,32 +76,45 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.font = windowGeometry.titleFont;
     ctx.textAlign = windowGeometry.titleFontAlign;
     ctx.fillStyle = windowGeometry.titleFontColor;
-    ctx.fillText('Ура вы победили!', 310, 40);
-    ctx.fillText('Список результатов:', 310, 60);
+    ctx.fillText('Ура вы победили!', windowGeometry.centerX, windowGeometry.titleFirstLineY);
+    ctx.fillText('Список результатов:', windowGeometry.centerX, windowGeometry.titleSecondLineY);
   }
 
   // Гистограмма
   function drawHistogram() {
     var columnStep = histoParameters.histoHeight / Math.round(window.tools.getMaxValue(times));
+    var userIndex;
 
+    // Отрисовка имен игроков
     function drawPlayersNames() {
-
-    }
-    function drawHistoColumns() {
-      for (var i = 0; i < times.length; i++) {
-        ctx.fillStyle = 'blue';
-        ctx.globalAlpha = window.tools.getRandomNumber(0.2, 1).toFixed(2);
-        drawSingleColumn(i, histoParameters);
-        ctx.globalAlpha = 1;
+      for (var i = 0; i < names.length; i++) {
+        var currentName = names[i];
+        if (currentName === histoParameters.playerName) {
+          userIndex = i;
+        }
       }
     }
+    // Отрисовка колонок
+    function drawHistoColumns(arr) {
+      for (var i = 0; i < times.length; i++) {
+        if (userIndex === i) {
+          ctx.fillStyle = arr.userColumnColor;
+        } else {
+          ctx.globalAlpha = window.tools.getRandomNumber(arr.opacityMin, arr.opacityMax).toFixed(arr.histoPrecision);
+          ctx.fillStyle = arr.otherPlayersColor;
+        }
+        drawSingleColumn(i, histoParameters);
+        ctx.globalAlpha = arr.opacityDefault;
+      }
+    }
+    // Отрисовка единичной колонки
     function drawSingleColumn(i, arr) {
-      var currTime = Math.round(times[i]);
-      ctx.fillRect(arr.histoStartX + i * (arr.columnWidth + arr.spaceBetweenColumns), arr.histoStartY, arr.columnWidth, -(currTime * columnStep.toFixed(3)));
+      var currentTime = Math.round(times[i]);
+      ctx.fillRect(arr.histoStartX + i * (arr.columnWidth + arr.spaceBetweenColumns), arr.histoStartY, arr.columnWidth, -(currentTime * columnStep.toFixed(arr.histoPrecision)));
     }
 
-    drawHistoColumns();
     drawPlayersNames();
+    drawHistoColumns(histoParameters);
   }
 
   drawStatisticsWindow();
